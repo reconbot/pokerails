f6e67f971fe3df2d45c8e36f5a5a985f2839a3e5
Francis Gulotta <wizard@roborooter.com>
2020-05-24 17:12:35 -0400

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+pokemon = CSV.read('./data/pokemon.csv', headers: true).map do |row|
+  row.to_h.slice("pokedex_number", "name", "japanese_name", "weight_kg")
+end
+ActiveRecord::Base.transaction do
+  pokemon.each do |pokemon|
+    pp pokemon unless pokemon["weight_kg"]
+    Pokemon.create(pokemon)
+  end
+end
