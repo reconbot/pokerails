9f372df087e6193474c40dbaaf0b34478ea90721
Francis Gulotta <wizard@roborooter.com>
2020-05-24 21:07:50 -0400

    Add a Pokemon graphql type

---
+++ app/graphql/types/pokemon_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class PokemonType < Types::BaseObject
+    field :id, ID, null: false
+    field :pokedex_number, Int, null: false
+    field :name, String, null: false
+    field :japanese_name, String, null: false
+    field :image, String, null: true
+    field :weight_kg, Float, null: false
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,9 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :pokemon, [Types::PokemonType], null: false, description: "All the Pokemon"
+    def pokemon
+      Pokemon.all
     end
   end
 end
